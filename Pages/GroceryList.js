0cd19a584798f283718594ff79dd9eed5d54636c
rnyakuti <rnyakuti@student.wethinkcode.co.za>
2020-07-17 18:00:36 +0200

    Stable working version

--- Pages/GroceryList.js
+++ Pages/GroceryList.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Header } from 'react-native-elements';
 import { Left, Right, Icon } from 'native-base';
+import { Checkbox } from 'react-native-paper';
 
 class GroceryListPage extends Component {
     render() {
         return (
             <View style={styles.container}>
                <Header
+                     backgroundColor ='black'
                     leftComponent={<Icon name="menu"  style={{color:'white'}}  onPress={() => this.props.navigation.openDrawer()} />}
                     centerComponent={<Text style={{color:'white'}}>Salwaa's Menu Planner</Text>}
                     rightComponent={<Icon name="cart"  style={{color:'white'}} onPress={() => this.props.navigation.openDrawer()}/>} //this will be the buy more recipes navigation
                 />
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text>Grocery List Page</Text>
-                </View>
+                
             </View>
         );
     }
--- Pages/HomePage.js
+++ Pages/HomePage.js
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet,Image } from 'react-native';
 import { Header } from 'react-native-elements';
-import { Icon,Container, Content, Card, CardItem,  Thumbnail } from 'native-base';
+import { Icon } from 'native-base';
+import { Card, Button  } from 'react-native-paper';
 
 class HomePage extends Component {
     render() {
         return (
             <View style={styles.container}>
                 <Header style ={{width: 10}}
+                    backgroundColor ='black'
                     leftComponent={<Icon name="menu"  style={{color:'white'}}  onPress={() => this.props.navigation.openDrawer()} />}
                     centerComponent={<Text style={{color:'white'}}>Salwaa's Menu Planner</Text>}
                     rightComponent={<Icon name="cart"  style={{color:'white'}} onPress={() => this.props.navigation.openDrawer()}/>} //this will be the buy more recipes navigation
                 />
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Container>
-                <Content>
-                    <Card >
-                        <CardItem>
-                            <Text>jjjjj Songs9</Text>
-                          
-                        </CardItem>
-
-                        <CardItem >
-                            <Image   style={{ height:100, width:100 }}  source={require('./../assets/home.jpg')} />
-                        </CardItem>
-
-                        <CardItem>
-                            <Icon name={'ios-musical-notes'} style={{color : '#ED4A6A'}} />
-                            <Text>Listen now</Text>
-                        </CardItem>
-                   </Card>
-                </Content>
-            </Container>
-                </View>
+              
+              <Card.Title
+                  title="Cape Malay and Other Recipes"
+                  subtitle="Plan your menu for the next few days/weeks"
+                  style={{alignItems: "center"}}
+             />
+             <Text style={{marginTop:-15,marginLeft: 15,marginBottom : 20, color:'black'}}>No more DAILY deciding for busy homes</Text>
+               
+                <Card>
+                    <Card.Cover  source={require('./../assets/home.jpg')} style={{height:400}} />
+                </Card>
+                <Button
+                    style={{ marginTop: 15, marginLeft:15, marginRight:15, color:'white' }}
+                    icon="send"
+                    mode="contained"
+                    color="#ffd13b"
+                   
+                >
+                    send Feedback
+                </Button>
               
             </View>
         );
