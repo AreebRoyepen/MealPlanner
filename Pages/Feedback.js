af26829123965fc66d4d1e47cd5bf072f73a44ab
rnyakuti <rnyakuti@student.wethinkcode.co.za>
2020-07-17 16:44:08 +0200

    test version

---
+++ Pages/Feedback.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Header } from 'react-native-elements';
+import { Left, Right, Icon } from 'native-base';
+import {Title,Paragraph,TextInput, Button }from 'react-native-paper';
+
+class FeedbackPage extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+               <Header
+                    leftComponent={<Icon name="menu"  style={{color:'white'}}  onPress={() => this.props.navigation.openDrawer()} />}
+                    centerComponent={<Text style={{color:'white'}}>Salwaa's Menu Planner</Text>}
+                    rightComponent={<Icon name="cart"  style={{color:'white'}} onPress={() => this.props.navigation.openDrawer()}/>} //this will be the buy more recipes navigation
+                />
+                <View style={{ }} >
+    
+    <Title style={{ marginTop: 15,marginLeft:15, marginRight:15 }}>
+      Send Us Your Feedback
+    </Title>
+    <Paragraph style={{ marginTop: 15,marginLeft:15, marginRight:15 }}>
+      We would love to hear from you! </Paragraph>
+    <TextInput
+      style={{ marginTop: 15,marginLeft:15, marginRight:15 }}
+      label='Subject'
+      mode='outlined'
+    />
+    <TextInput
+      style={{ marginTop: 15, height:250, marginLeft:15, marginRight:15}}
+      label='Your message goes here...'
+      mode='flat'
+    />
+     <Button
+    style={{ marginTop: 15, marginLeft:15, marginRight:15 }}
+    icon="send"
+    mode="contained"
+  >
+    send Feedback
+  </Button>
+
+    </View>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
+export default  FeedbackPage;
+
+
+
+
