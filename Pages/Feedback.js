ce25101052ff97bb0ccdfb9d09d459415ede53ea
Areeb <areeb.royepen@gmail.com>
2020-07-24 16:05:18 +0200

    feedback cleanup

--- Pages/Feedback.js
+++ Pages/Feedback.js
@@ -1,76 +1,68 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Header } from 'react-native-elements';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { Left, Right, Icon } from 'native-base';
-import {Title,Paragraph,TextInput, Button }from 'react-native-paper';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, KeyboardAvoidingView, Platform, ScrollView } from "react-native";
+import { Header } from "react-native-elements";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { Title, Paragraph, TextInput, Button } from "react-native-paper";
 
-import HeaderButton from '../components/HeaderButton';
+import HeaderButton from "../components/HeaderButton";
+import DefaultTextInput from "../components/DefaultTextInput";
+import DefaultTextArea from "../components/DefaultTextArea";
+import Colors from "../constants/Colors";
 
-const  FeedbackPage = () => {
-        return (
-            <View style={styles.container}>
-               {/* <Header
-                   backgroundColor ='black'
-                    leftComponent={<Icon name="menu"  style={{color:'white'}}  onPress={() => this.props.navigation.openDrawer()} />}
-                    centerComponent={<Text style={{color:'white'}}>Salwaa's Menu Planner</Text>}
-                    rightComponent={<Icon name="cart"  style={{color:'white'}} onPress={() => this.props.navigation.openDrawer()}/>} //this will be the buy more recipes navigation
-                /> */}
-                <View style={{ }} >
+const FeedbackPage = () => {
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS == "ios" ? "padding" : "height"} keyboardVerticalOffset = {90}>
+    <ScrollView>
     
-    <Title style={{ marginTop: 15,marginLeft:15, marginRight:15 }}>
-      Send Us Your Feedback
-    </Title>
-    <Paragraph style={{ marginTop: 15,marginLeft:15, marginRight:15 }}>
-      We would love to hear from you! </Paragraph>
-    <TextInput
-      style={{ marginTop: 15,marginLeft:15, marginRight:15 }}
-      label='Subject'
-      mode='outlined'
-    />
-    <TextInput
-      style={{ marginTop: 15, height:250, marginLeft:15, marginRight:15}}
-      label='Your message goes here...'
-      mode='flat'
-    />
-     <Button
-    style={{ marginTop: 15, marginLeft:15, marginRight:15 }}
-    icon="send"
-    mode="contained"
-    color="#ffd13b"
-  >
-    send Feedback
-  </Button>
-
+    <View >
+        <Title style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }}>
+          Send Us Your Feedback
+        </Title>
+        <Paragraph style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }}>
+          We would love to hear from you!
+        </Paragraph>
+        <DefaultTextInput
+          label="Subject"
+        />
+        <DefaultTextArea
+          label="Message"
+          rows={5}
+        />
+        <Button
+          style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }}
+          icon="send"
+          mode="contained"
+          color={Colors.buttonColor}
+        >
+          Send Feedback
+        </Button>
     </View>
-            </View>
-        );
-}
+    
+    </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
 
-FeedbackPage.navigationOptions = navData =>{
+FeedbackPage.navigationOptions = (navData) => {
   return {
-      headerTitle: "Feedback",
-      headerLeft: () =>
-        <HeaderButtons HeaderButtonComponent={HeaderButton}>
-          <Item
-            title="Menu"
-            iconName="ios-menu"
-            onPress={() => {
-              navData.navigation.toggleDrawer();
-            }}
-          />
-        </HeaderButtons>
-      
-    };
-}
+    headerTitle: "Feedback",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1,
+  },
 });
-export default  FeedbackPage;
-
-
-
-
+export default FeedbackPage;
