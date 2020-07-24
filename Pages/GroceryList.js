da36117be2ab0e65a7d0a3e199bf37c16a8a2251
Areeb <areeb.royepen@gmail.com>
2020-07-24 16:03:11 +0200

    grocery list

--- Pages/GroceryList.js
+++ Pages/GroceryList.js
@@ -1,46 +1,114 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { Header } from 'react-native-elements';
-import { Left, Right, Icon } from 'native-base';
-import { Checkbox } from 'react-native-paper';
+import React, { Component, useEffect, useState, useCallback } from "react";
+import { View, Text, StyleSheet, FlatList, ActivityIndicator } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import HeaderButton from '../components/HeaderButton';
+import HeaderButton from "../components/HeaderButton";
+import { useSelector } from "react-redux";
+//import {  } from "react-native-gesture-handler";
 
-const GroceryListPage = () => {
-        return (
-            <View style={styles.container}>
-               {/* <Header
-                     backgroundColor ='black'
-                    leftComponent={<Icon name="menu"  style={{color:'white'}}  onPress={() => this.props.navigation.openDrawer()} />}
-                    centerComponent={<Text style={{color:'white'}}>Salwaa's Menu Planner</Text>}
-                    rightComponent={<Icon name="cart"  style={{color:'white'}} onPress={() => this.props.navigation.openDrawer()}/>} //this will be the buy more recipes navigation
-                /> */}
-                
-            </View>
-        );
-}
+import {fetchInventory} from "../api/Api";
+import { ScrollView } from "react-native-gesture-handler";
+import ListItem from "../components/ListItem";
 
-GroceryListPage.navigationOptions = navData =>{
-    return {
-        headerTitle: "Grocery List",
-        headerLeft: () =>
-          <HeaderButtons HeaderButtonComponent={HeaderButton}>
-            <Item
-              title="Menu"
-              iconName="ios-menu"
-              onPress={() => {
-                navData.navigation.toggleDrawer();
-              }}
-            />
-          </HeaderButtons>
-        
-      };
-}
+const GroceryListPage = props => {
+  
+  const recipes = useSelector((state) => state.recipes.selectedRecipes);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const [groceries, setGroceries] = useState({})
+
+  const getList = useCallback (async() => {
+
+    let param = "";
+    for (let x of recipes){
+      param = param + x.id + ","
+    }
+
+    setIsRefreshing(true)
+      try{
+        let resp = await fetchInventory(param);
+        let list = [];
+
+        list  =  Object.entries(resp) ; //fuck yes
+
+        console.log(list)
+        setGroceries(list)
+
+        setError(false);
+      }catch(err){
+        setError(true);
+      }finally{
+        setIsRefreshing(false)
+      }
+    
+  },[recipes]);
+
+  useEffect(() => {
+
+    const willFocusSub = props.navigation.addListener("willFocus", getList);
+
+    return () => {
+      willFocusSub.remove();
+    };
+  }, [getList]);
+
+  useEffect(() => {
+
+    setIsLoading(true);
+    getList();
+    setIsLoading(false)
+
+
+  }, [getList])
+
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color={Colors.primaryColor} />
+      </View>
+    );
+  }
+
+  return (
+    <ScrollView>
+    <View style={styles.container}>
+
+      <FlatList
+        onRefresh={getList}
+        refreshing={isRefreshing}
+        data={groceries}
+        keyExtractor={(item, index) => item[0]}
+        renderItem={(x) => <ListItem>{x.item[0] + " " + x.item[1]}</ListItem>}
+        style={{ width: "100%" }}
+      />
+    </View>
+    </ScrollView>
+  );
+};
+
+GroceryListPage.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Grocery List",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1,
+  },
 });
-export default  GroceryListPage;
+export default GroceryListPage;
