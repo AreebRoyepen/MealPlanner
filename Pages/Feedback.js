87809aade821dd5bad83486d134c8faf02293ed1
Areeb <areeb.royepen@gmail.com>
2020-07-27 14:37:43 +0200

    feedback done

--- Pages/Feedback.js
+++ Pages/Feedback.js
@@ -1,44 +1,77 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, KeyboardAvoidingView, Platform, ScrollView } from "react-native";
-import { Header } from "react-native-elements";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Linking,
+  TextInput,
+  Text,
+  Button,
+} from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { Title, Paragraph, TextInput, Button } from "react-native-paper";
+import { Title, Paragraph } from "react-native-paper";
 
 import HeaderButton from "../components/HeaderButton";
-import DefaultTextInput from "../components/DefaultTextInput";
-import DefaultTextArea from "../components/DefaultTextArea";
 import Colors from "../constants/Colors";
 
 const FeedbackPage = () => {
+  const [subject, setSubject] = useState();
+  const [message, setMessage] = useState();
+
   return (
-    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS == "ios" ? "padding" : "height"} keyboardVerticalOffset = {90}>
-    <ScrollView>
-    
-    <View >
-        <Title style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }}>
-          Send Us Your Feedback
-        </Title>
-        <Paragraph style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }}>
-          We would love to hear from you!
-        </Paragraph>
-        <DefaultTextInput
-          label="Subject"
-        />
-        <DefaultTextArea
-          label="Message"
-          rows={5}
-        />
-        <Button
-          style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }}
-          icon="send"
-          mode="contained"
-          color={Colors.buttonColor}
-        >
-          Send Feedback
-        </Button>
-    </View>
-    
-    </ScrollView>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={90}
+    >
+      <ScrollView>
+        <View>
+          <Title style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }}>
+            Send Us Your Feedback
+          </Title>
+          <Paragraph style={{ marginTop: 15, marginLeft: 15, marginRight: 15 }}>
+            We would love to hear from you!
+          </Paragraph>
+
+          <View style={styles.formControl}>
+            <Text style={styles.label}>Subject</Text>
+            <TextInput
+              multiline
+              style={styles.input}
+              onChangeText={(e) => setSubject(e)}
+              //value={description}
+              //onChangeText={text => setDescription(text)}
+            />
+          </View>
+
+          <View style={styles.formControl}>
+            <Text style={styles.label}>Message</Text>
+            <TextInput
+              multiline
+              style={styles.input}
+              onChangeText={(e) => setMessage(e)}
+              //value={description}
+              //onChangeText={text => setDescription(text)}
+            />
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <Button
+              icon="send"
+              mode="contained"
+              color={Colors.buttonColor}
+              onPress={() =>
+                Linking.openURL(
+                  `mailto:areeb.royepen@gmail.com?subject=${subject}&body=${message}`
+                )
+              }
+              title="Send Feedback"
+            />
+          </View>
+        </View>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
@@ -63,6 +96,25 @@ FeedbackPage.navigationOptions = (navData) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  formControl: {
+    width: "100%",
+  },
+  label: {
+    fontFamily: "open-sans-bold",
+    marginVertical: 8,
+    paddingHorizontal: 10,
+  },
+  input: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  buttonContainer: {
+    marginTop: 15,
+    marginLeft: 15,
+    marginRight: 15,
   },
 });
 export default FeedbackPage;
