85262de5724c5892a391514685ede3f289afc51c
Areeb <areeb.royepen@gmail.com>
2020-08-03 14:51:24 +0200

    clean up

--- Pages/Recipes.js
+++ Pages/Recipes.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../components/HeaderButton";
 import RecipeList from "../components/RecipeList";
+import { useDispatch } from "react-redux";
+
+import * as recipeActions from "../store/actions/recipes"
 
 const RecipesPage = (props) => {
-  
-    const [search, setSearch] = useState();
 
+  const dispatch = useDispatch()
+  
+  useEffect(()=>{
+    dispatch(recipeActions.getRecipes());
+  }, [dispatch])
 
   return (
     <View style={styles.container}>
