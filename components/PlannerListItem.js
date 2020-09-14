3a89c33c32a4dc9ec20a591288b8a3099a9de645
Areeb <areeb.royepen@gmail.com>
2020-09-14 15:48:23 +0200

    update planner list item

--- components/PlannerListItem.js
+++ components/PlannerListItem.js
@@ -28,131 +28,19 @@ const PlannerListItem = (props) => {
               <Text style={styles.titleText}>{props.title}</Text>
             </Body>
           </Left>
-          <Text style={styles.dinnerColor}>{props.time}</Text>
+          <Text style={styles.mealColor}>{props.time}</Text>
         </CardItem>
       </Card>
-      <Modal
-        style={styles.modalView}
-        animationType="slide"
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>
-              Are you sure you want to delete?
-            </Text>
-
-            <TouchableHighlight
-              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
-              onPress={() => {
-                setModalVisible(!modalVisible);
-              }}
-            >
-              <Text style={styles.textStyle}>confirm</Text>
-            </TouchableHighlight>
-            <TouchableHighlight
-              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
-              onPress={() => {
-                setModalVisible(!modalVisible);
-              }}
-            >
-              <Text style={styles.textStyle}>cancel</Text>
-            </TouchableHighlight>
-          </View>
-        </View>
-      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  plannerListItem: {
-    height: "200%",
-    width: "100%",
-    backgroundColor: "#f5f5f5",
-    borderRadius: 10,
-    overflow: "hidden",
-    marginVertical: 10,
-  },
   titleText: {
     fontWeight: "bold",
   },
-  bgImage: {
-    width: "100%",
-    height: "100%",
-    justifyContent: "flex-end",
-  },
-  row: {
-    flexDirection: "row",
-  },
-  header: {
-    height: "85%",
-  },
-  details: {
-    paddingHorizontal: 10,
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "15%",
-  },
-  dinnerColor: {
+  mealColor: {
     color: Colors.accentColor,
-  },
-  titleContainer: {
-    backgroundColor: "rgba(0,0,0,0.5)",
-    paddingVertical: 5,
-    paddingHorizontal: 12,
-  },
-  title: {
-    fontFamily: "open-sans-bold",
-    fontSize: 20,
-    color: "white",
-    textAlign: "center",
-  },
-  buttonContainer: {
-    //width: "20%",
-    alignItems: "center",
-    backgroundColor: "transparent",
-  },
-
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    opacity: 0.9,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  openButton: {
-    backgroundColor: "#F194FF",
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
   },
 });
 
